1ebd020761ee2e1bf11b9fdcdcb4c0c1ab11b508
Venakteswarlu-500apps <venkateswarlu.tiruveedula@mantragroup.com>
2022-04-12 19:35:28 +0530

    task(12-04-2022)

---
+++ myfile/src/components/bcard.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+
+  <h4>Top and Bottom</h4>
+    <b-card-group deck>
+      <b-card img-src="https://placekitten.com/1000/300" img-alt="Card image" img-top>
+        <b-card-text>
+          Some quick example text to build on the card and make up the bulk of the card's content.
+        </b-card-text>
+      </b-card>
+
+      <b-card img-src="https://placekitten.com/1000/300" img-alt="Card image" img-bottom>
+        <b-card-text>
+          Some quick example text to build on the card and make up the bulk of the card's content.
+        </b-card-text>
+      </b-card>
+    </b-card-group>
+</div>
+
+</template>
+<script>
+export default {
+    name:'VenkY',
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
