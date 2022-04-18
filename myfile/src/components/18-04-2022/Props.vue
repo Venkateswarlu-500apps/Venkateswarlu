e284a93052ef59088488aed8bd6aefeb47c1e34a
Venakteswarlu-500apps <venkateswarlu.tiruveedula@mantragroup.com>
2022-04-18 19:11:14 +0530

    task(18-04-2022)

---
+++ myfile/src/components/18-04-2022/Props.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>This is props child</h3>
+    <h1>message:{{ msg }}</h1>
+    <h1>name:{{ name }}</h1>
+    <h1>number:{{ number }}</h1>
+  </div>
+</template>
+<script>
+export default {
+    name:"PropS",
+            props:{
+                msg:String,
+                name:String,
+                number:{
+                default:1234345,
+                type:Number
+                },
+            }
+        }
+</script>
---
+++ myfile/src/components/18-04-2022/Props1.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h4>msg:{{ message }}</h4>
+    <h4>name:{{ name }}</h4>
+    <h4>number:{{ number }}</h4>
+    <h4>Items:{{ Items }}</h4>
+    <i><b><h3>mantra:{{ mantra }}</h3></b></i>
+  </div>
+</template>
+<script>
+export default {
+  name: "propS",
+  components: {},
+  props: {
+    message: String,
+    name: String,
+    number: { default: 1177, type: Number },
+    mantra: String,
+    Items: [
+      { age: 23, firstname: "venkatesh", lastname: "tiruveedula" },
+      { age: 24, firstname: "pawan", lastname: "kumar" },
+    ],
+  },
+  methods: {
+    fun() {
+      alert("keep it up");
+    },
+  },
+};
+</script>
