499147a8e409ce490d16dda308da6dc3d0cb01de
Venakteswarlu-500apps <venkateswarlu.tiruveedula@mantragroup.com>
2022-04-20 19:05:25 +0530

    task(20-04-2022)

---
+++ myfile/src/components/20-04-2022 Assignment/Question3.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Set Time Out</h1>
+    <p>{{ time }}</p>
+    <button @click="fun()">click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VenkY",
+  
+  data() {
+    return {
+      time: "",
+    };
+  },
+
+  methods: {
+    fun() {
+      setTimeout(() => {
+        this.time = new Date();
+      }, 5000);
+
+      setInterval(() => {
+        this.time = new Date();
+      }, 6000);
+    },
+  },
+};
+</script>
---
+++ myfile/src/components/20-04-2022 Assignment/Question9.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <center>
+        <h1>b-form</h1>
+        <b-form name="form" @submit="fun()">
+        
+        NAME:<b-form-input type="text"  name="name" placeholder="Enter Your Name" required class="w-25"></b-form-input><br><br>
+
+        EMAIL:<b-form-input type="email"  name="email" placeholder="Enter Your Email" required class="w-25"></b-form-input><br><br>
+
+        Age:<b-form-input type="number"  name="number" placeholder="Enter Your Age" required class="w-25"></b-form-input><br><br>
+
+        Contact Number:<b-form-input type="number"  name="Cnumber" placeholder="Enter Your Number" required class="w-25"></b-form-input><br><br>
+
+        <b-button type="submit" variant="primary" value="Login">Submit</b-button>
+
+        </b-form>
+        </center>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'VenkY',
+    data(){
+        return{
+        }
+    },
+    methods:{
+        fun(){
+        document.write("NAME:"+"&nbsp"+document.form.name.value+"<br>"+"<br>"+"EMAIL:"+"&nbsp"+document.form.email.value+
+        "<br>"+"<br>"+"Age:"+"&nbsp"+document.form.number.value+"<br>"+"<br>"+"Contact Number:"+"&nbsp"+document.form.Cnumber.value);
+        }
+    }
+}
+</script>
