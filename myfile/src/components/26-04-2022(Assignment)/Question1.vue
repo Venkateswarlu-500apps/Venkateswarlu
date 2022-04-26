2f6a370b29a0ecf3e8c74aa3a7fdaa823f0b4a39
Venakteswarlu-500apps <venkateswarlu.tiruveedula@mantragroup.com>
2022-04-26 19:37:18 +0530

    task(26-04-2022)

---
+++ myfile/src/components/26-04-2022(Assignment)/Question1.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Displaying users list in alphabetical and reverse order in UI </h1>
+         <button @click="start()">Alphabatical Order</button><br><br>
+         <button @click="runreverse()">Reverse Order</button><br><br>
+         {{res}}<br><br>
+         {{res1}}
+    </div>
+</template>
+<script>
+export default{
+    name:'VenkY',
+    data(){
+        return{
+         Users:[{name:'500apps',id:1},
+                {name:'Technologies',id:2},
+                {name:'Agile',id:3},
+                {name:'Mantra',id:4},
+                {name:'Clockly',id:5},
+                {name:'CRM',id:6},
+                {name:'500apps',id:7}],
+         field:['name'],
+         res:" ",
+         res1:" "        
+        }
+    },
+    methods:{
+        start(){
+        this.res=this.Users.sort((a,b) => (a[this.field] || "").toString().localeCompare((b[this.field] || "").toString()));
+        document.write(JSON.stringify(this.res))
+        },
+        runreverse(){
+        this.res1=this.Users.sort((a,b) => (a[this.field] || "").toString().localeCompare((b[this.field] || "").toString()));
+        document.write(JSON.stringify(this.res1.reverse()))
+        }
+    }
+}
+</script>
---
+++ myfile/src/components/26-04-2022(Assignment)/Question3.vue
@@ -0,0 +1,67 @@
+<template>
+  <div align="center">
+      <h1>CRUD Operations</h1>
+    <b-row class="w-50">
+        <b-col>
+        <b-card>
+          <b-form id="form">
+            Enter First Name:<b-form-input id="sample1" v-model="value1" placeholder="enter Firstname"></b-form-input>
+            Enter LastName:<b-form-input id="sample2" v-model="value2" placeholder="enter Lastname"></b-form-input>
+
+            <b-button @click="Add()">Add</b-button>
+
+            <b-button @click="Delete()">Delete</b-button>
+
+            <b-button @click="Update()">Update</b-button>
+          </b-form>
+
+          <b-table striped hover :items="items" :fields="fields"></b-table> 
+          </b-card>
+          </b-col>
+          </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VenkY",
+  data() {
+    return {
+      items: [{ firstname: "", lastname: "" }],
+
+      fields: ["firstname", "lastname"],
+
+      value1: "",
+    };
+  },
+  methods: {
+    Add() {
+      this.items.push({
+        firstname: document.getElementById("sample1").value,
+
+        lastname: document.getElementById("sample2").value,
+      });
+    },
+    Delete() {
+      this.items.pop();
+    },
+    Update() {
+        this.items.update({
+        firstname: document.getElementById("sample1").value,
+
+        lastname: document.getElementById("sample2").value, 
+        })
+    },
+    
+  },
+};
+</script>
+
+
+<style scoped>
+div {
+  align-content: center;
+
+  padding-left: 45px;
+}
+</style>
