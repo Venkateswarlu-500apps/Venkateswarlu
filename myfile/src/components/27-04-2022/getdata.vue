67f099eec3e86ae1ba440ff4f18e500a7c70ace2
Venakteswarlu-500apps <venkateswarlu.tiruveedula@mantragroup.com>
2022-04-27 19:16:36 +0530

    task(27-04-2022)

---
+++ myfile/src/components/27-04-2022/getdata.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <center>
+    <h3>fetch api</h3>
+    <b-form-input v-model="value" placeholder="Enter country name"></b-form-input>
+     <b-button @click="getData()" variant="success">click</b-button>
+    <b-table class="w-75" striped hover :items="posts" :fields="fields"></b-table>
+    </center>
+  </div>
+</template>
+<script>
+export default {
+  name: "FetchapI",
+  data() {
+    return {
+      posts: " ",
+      fields: ["name", "country", "web_pages","domains"],
+    };
+  },
+  methods: {
+    async getData() {
+      try {
+        let response = await fetch("http://universities.hipolabs.com/search?country=" + this.value);
+        this.posts = await response.json();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.getData();
+  },
+};
+</script>
+<style>
---
+++ myfile/src/components/27-04-2022/task.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <b-form-input v-model="text" placeholder="Enter country" required></b-form-input><br />
+        <b-button @click="fun" variant="success">Click</b-button>
+        <b-table :items="res" :fields="fields"></b-table>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default{
+    name:'VenkY',
+    data(){
+        return{
+          res:"",
+          fields:["name", "country", "web_pages","domains"]
+        }
+    },
+    methods:{
+       async fun(){
+        let response = await axios.get("http://universities.hipolabs.com/search?country="+ this.text);
+        this.res = await response.json(); 
+        },
+    },
+    created(){
+        this.fun();
+    }
+}
+</script>
