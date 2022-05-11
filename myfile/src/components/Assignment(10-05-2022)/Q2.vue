abe4df39945753befe923d8fc994a3ae482289ff
Venakteswarlu-500apps <venkateswarlu.tiruveedula@mantragroup.com>
2022-05-11 10:43:59 +0530

    10-05-2022

---
+++ myfile/src/components/Assignment(10-05-2022)/Q2.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>Watch</h1>
+        <center>
+        <b-card class="w-50">
+        <b>TextBox:1</b>
+        <input type="text" v-model="tb1"/>&nbsp;
+        <b>TextBox:2</b>
+        <input type="'text' required readonly value='watch'" v-model="tb2"/>
+        </b-card>
+        </center>
+    </div>
+</template>
+<script>
+
+export default {
+    name:'VenkY',
+    data(){
+        return{
+            tb1:' ',
+            tb2:' '
+        }
+    },
+    watch:{
+        tb1:function(value){
+            this.tb1 = value;
+            this.tb2 = value+this.tb1;
+        },
+        tb2:function(value){
+            this.tb2 = value;
+            this.tb1 = value-this.tb2;
+        }
+    }
+}
+</script>
