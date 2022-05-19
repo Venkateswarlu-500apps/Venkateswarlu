717d2587fb44d8214e4b654227047b530c186797
Venakteswarlu-500apps <venkateswarlu.tiruveedula@mantragroup.com>
2022-05-19 20:45:51 +0530

    task(19-05-2022)

---
+++ myfile/src/components/NOTE/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+      <center>
+      <b-form class="w-50">
+          Title:<b-form-input></b-form-input>
+          Content:<b-form-textarea></b-form-textarea>
+      </b-form><br>
+      </center>
+      <b-button type="submit" @click="newPad" variant="success"><b>+</b></b-button>&nbsp;
+      <b-button type="submit" @click="savePad" variant="primary">Save</b-button>&nbsp;
+      <b-button type="submit" @click="deletePad" variant="danger">Delete</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"HomePage",
+    data(){
+        return{
+            page:[ ],
+            pageNo:0
+        }
+    },
+    methods:{
+        newPad(){
+            this.page.push({
+                Title:' ',
+                Content:' '
+            })
+            this.pageNo = this.page.length - 1
+        },
+        changePad(pageNo){
+            this.pageNo = pageNo
+        },
+        savePad(){
+
+        },
+        deletePad(){
+            this.page.splice(this.pageNo,1)
+            this.pageNo = Math.max(this.index -1, 0)
+        }
+       
+    }
+}
+</script>
+
+<style>
+
+</style>
