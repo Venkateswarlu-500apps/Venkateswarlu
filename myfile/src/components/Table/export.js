cd163f7640274ded65aa36d110db3daf8c907906
Venkateswarlu-500apps <103184203+Venkateswarlu-500apps@users.noreply.github.com>
2022-06-04 13:51:43 +0530

    Delete export.js

--- myfile/src/components/Table/export.js
+++
@@ -1,7 +0,0 @@
-import exportFromJSON from 'export-from-json'
-
-const data = [{ foo: 'foo'}, { bar: 'bar' }]
-const fileName = 'download'
-const exportType =  exportFromJSON.types.csv
-
-exportFromJSON({ data, fileName, exportType })
